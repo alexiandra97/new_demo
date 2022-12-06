6e5ec42ac66b291c3305a9ab4220e7ed396f47a3
alexiandra97 <vitkovacaleksandra@gmail.com>
2022-12-06 01:01:03 +0100

    fibonacci recursion

---
+++ fibonacci.py
@@ -0,0 +1,26 @@
+def fibonacciiter(n):
+	lista=[0,1]
+	f=0
+	for i in range(1,n):
+		f = lista[i]+lista[i-1]
+		lista.append(f)
+	for i in lista:
+		if lista[n]==i:
+			return i
+# So we don't have to get whole array just value of index member
+def fibonaccieiter2(n):
+	list2=[0,1]
+	for i in range(2,n+1):
+		list2.append(list2[i-2]+list2[i-1])
+	return list2[n]
+
+def fibonaccirecursive(n):
+	if n<2:
+		return n
+	return fibonaccirecursive(n-1)+fibonaccirecursive(n-2)
+	#O(2^n)
+	
+
+print(fibonaccieiter2(19))
+print(fibonacciiter(19))
+print(fibonaccirecursive(19))
